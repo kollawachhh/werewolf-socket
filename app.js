acf01d23f63ced0cb0ffd360c386457e855e8836
Kollawach Chat <Kollawach.s@ku.th>
2023-04-02 01:29:38 +0700

    fix error for deploy 4

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const server = require("http").createServer();
+const path = require("path");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+//set static folder
+app.use(express.static(path.join("public")));
+
 const io = require("socket.io")(server, {
   cors: {
     origin: "https://werewolfg.netlify.app",
